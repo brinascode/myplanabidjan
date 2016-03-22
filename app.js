6830dad79415798bb4b96e2e4450580c47516d41
lollipopchou <sabrina.koumoin@gmail.com>
2016-03-22 11:36:27 +0000

    hooo

--- app.js
+++ app.js
@@ -19,16 +19,17 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+//view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(favicon(path.join(__dirname, 'public', "images",'favicon.ico')));
 
 //Added to generator: to handle Angular routing I think
