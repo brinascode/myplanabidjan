f980bf19ea40d31e14a5425789d72929ad607bb0
lollipopchou <sabrina.koumoin@gmail.com>
2016-03-23 12:35:40 +0000

    pol

--- routes/index.js
+++ routes/index.js
@@ -10,9 +10,9 @@ var assert = require("assert")
 
 
 //Getting our different mongoose models 
-var Restaurant = require("../Schemas/Restaurants-Schema")
-var Magasin = require("../Schemas/Magasin")
-var Avis = require("../Schemas/Avis")
+var Restaurant = require("../Schemas/Restaurants-Schema.js")
+var Magasin = require("../Schemas/Magasin.js")
+var Avis = require("../Schemas/Avis.js")
 
 //******************************************** Restaurants *****************************************
 
